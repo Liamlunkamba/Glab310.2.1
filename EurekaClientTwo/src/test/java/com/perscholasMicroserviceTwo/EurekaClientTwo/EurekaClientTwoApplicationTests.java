85adc41244ccdcc8801409e99d65a10b07dc42c8
Liamlunkamba <liamlunkamba12@gmail.com>
2024-09-05 11:47:32 -0400

    we still commiting

---
+++ EurekaClientTwo/src/test/java/com/perscholasMicroserviceTwo/EurekaClientTwo/EurekaClientTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perscholasMicroserviceTwo.EurekaClientTwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClientTwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
